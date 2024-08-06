a49255bcc18d25763a56f564df956571109b1681
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-08-06 17:15:49 +0700

    receipt

--- src/pages/Receipt/BookingReceipt.jsx
+++ src/pages/Receipt/BookingReceipt.jsx
@@ -66,133 +66,138 @@ const BookingReceipt = () => {
   }
 
   return (
-    <Container style={{ marginTop: "20px" }}>
-      <Card style={{ width: "100%", margin: "0 auto", paddingTop: "0", border: "1px solid #ccc" }}>
-        <Title level={3} style={{ textAlign: "center", marginTop: "0" }}>{`Biên Lai Mượn Phòng ${room.room_id}`}</Title>
-        <Divider />
-        <Row className="receipt">
-          <Col>
-            <Text strong>Tên:</Text>
-            <Text>{` ${name}`}</Text>
-            <br />
-            <Text strong>Trạng thái:</Text>
-            <Text>{` ${status}`}</Text>
-            <br />
-            <Text strong>Ngày tạo:</Text>
-            <Text>{` ${formatDateTime(created_at)}`}</Text>
-            <br />
-            <Text strong>Ngày mượn:</Text>
-            <Text>{` ${formatDate(date_booking)}`}</Text>
-            <br />
-            <Text strong>Thời gian:</Text>
-            <Text>{` ${start_time} - ${end_time}`}</Text>
-            <br />
-            <Text strong>Số người sử dụng:</Text>
-            <Text>{` ${total_customer} người`}</Text>
-            <br />
-            <Text strong>Ghi chú:</Text>
-            <Text>{` ${note}`}</Text>
-          </Col>
-          <Col className="qr-code-custom">
-            <QRCode value={qrDataUrl} size={75} />
-          </Col>
-        </Row>
-        <Divider />
-        <Row>
-          <Col span={24}>
-            <Title level={4} style={{ marginTop: "0" }}>
-              Bên Thuê Phòng
-            </Title>
-            <Text strong>MSSV:</Text>
-            <Text>{` ${customer.id}`}</Text>
-            <br />
-            <Text strong>Họ và Tên:</Text>
-            <Text>{` ${customer.full_name}`}</Text>
-            <br />
-            <Text strong>Khoa:</Text>
-            <Text>{` ${customer.department}`}</Text>
-            <br />
-            <Text strong>Số điện thoại:</Text>
-            <Text>{` ${customer.phone_number}`}</Text>
-            <br />
-            <Text strong>Email:</Text>
-            <Text>{` ${customer.email}`}</Text>
-            <br />
-            <Text strong>Địa chỉ:</Text>
-            <Text>{` ${customer.address}`}</Text>
-          </Col>
-        </Row>
-        <Divider />
-        <Row>
-          <Col span={24}>
-            <Title level={4} style={{ marginTop: "0" }}>
-              Bên Cho Thuê
-            </Title>
-            <Text strong>Người cho mượn:</Text>
-            <Text>{` ${user.full_name}`}</Text>
-            <br />
-            <Text strong>Email:</Text>
-            <Text>{` ${user.email}`}</Text>
-            <br />
-            <Text strong>Số điện thoại:</Text>
-            <Text>{` ${user.profile.phone_number}`}</Text>
-          </Col>
-        </Row>
-        <Divider />
-        <Row>
-          <Col span={24}>
-            <Title level={4} style={{ marginTop: "0" }}>
-              Chi Tiết Phòng
-            </Title>
-            <Text strong>Tên tòa nhà:</Text>
-            <Text>{` ${room.house_name}`}</Text>
-            <br />
-            <Text strong>Tên phòng:</Text>
-            <Text>{` ${room.room_id}`}</Text>
-            <br />
-            <Text strong>Loại phòng:</Text>
-            <Text>{` ${room.category}`}</Text>
-            <br />
-            <Text strong>Tài nguyên:</Text>
-            <ul>
-              {room.detail.map((item, index) => (
-                <li key={index}>{`${item.name}: ${item.total}`}</li>
-              ))}
-            </ul>
-            <Text strong>Ghi chú:</Text>
-            <Text>{` ${room.note}`}</Text>
-          </Col>
-        </Row>
-        <Divider />
-        <Row>
-          <Col span={8} style={{ textAlign: "center" }}>
-            <Text strong>Ký tên:</Text>
-            <br />
-            <Text>{user.full_name}</Text>
-            <br />
-            <Text>(Bên cho thuê)</Text>
-          </Col>
-          <Col span={8}></Col>
-          <Col span={8} style={{ textAlign: "center" }}>
-            <Text strong>Ký tên:</Text>
-            <br />
-            <Text>{customer.full_name}</Text>
-            <br />
-            <Text>(Bên thuê phòng)</Text>
-          </Col>
-        </Row>
+    <div className="receipt-container">
+      <Container style={{ marginTop: "20px" }}>
+        <Card style={{ width: "100%", margin: "0 auto", paddingTop: "0", border: "1px solid #ccc" }}>
+          <Title
+            level={3}
+            style={{ textAlign: "center", marginTop: "0" }}
+          >{`Biên Lai Mượn Phòng ${room.room_id}`}</Title>
+          <Divider />
+          <Row className="receipt">
+            <Col>
+              <Text strong>Tên:</Text>
+              <Text>{` ${name}`}</Text>
+              <br />
+              <Text strong>Trạng thái:</Text>
+              <Text>{` ${status}`}</Text>
+              <br />
+              <Text strong>Ngày tạo:</Text>
+              <Text>{` ${formatDateTime(created_at)}`}</Text>
+              <br />
+              <Text strong>Ngày mượn:</Text>
+              <Text>{` ${formatDate(date_booking)}`}</Text>
+              <br />
+              <Text strong>Thời gian:</Text>
+              <Text>{` ${start_time} - ${end_time}`}</Text>
+              <br />
+              <Text strong>Số người sử dụng:</Text>
+              <Text>{` ${total_customer} người`}</Text>
+              <br />
+              <Text strong>Ghi chú:</Text>
+              <Text>{` ${note}`}</Text>
+            </Col>
+            <Col className="qr-code-custom">
+              <QRCode value={qrDataUrl} size={75} />
+            </Col>
+          </Row>
+          <Divider />
+          <Row>
+            <Col span={24}>
+              <Title level={4} style={{ marginTop: "0" }}>
+                Bên Thuê Phòng
+              </Title>
+              <Text strong>MSSV:</Text>
+              <Text>{` ${customer.id}`}</Text>
+              <br />
+              <Text strong>Họ và Tên:</Text>
+              <Text>{` ${customer.full_name}`}</Text>
+              <br />
+              <Text strong>Khoa:</Text>
+              <Text>{` ${customer.department}`}</Text>
+              <br />
+              <Text strong>Số điện thoại:</Text>
+              <Text>{` ${customer.phone_number}`}</Text>
+              <br />
+              <Text strong>Email:</Text>
+              <Text>{` ${customer.email}`}</Text>
+              <br />
+              <Text strong>Địa chỉ:</Text>
+              <Text>{` ${customer.address}`}</Text>
+            </Col>
+          </Row>
+          <Divider />
+          <Row>
+            <Col span={24}>
+              <Title level={4} style={{ marginTop: "0" }}>
+                Bên Cho Thuê
+              </Title>
+              <Text strong>Người cho mượn:</Text>
+              <Text>{` ${user.full_name}`}</Text>
+              <br />
+              <Text strong>Email:</Text>
+              <Text>{` ${user.email}`}</Text>
+              <br />
+              <Text strong>Số điện thoại:</Text>
+              <Text>{` ${user.profile.phone_number}`}</Text>
+            </Col>
+          </Row>
+          <Divider />
+          <Row>
+            <Col span={24}>
+              <Title level={4} style={{ marginTop: "0" }}>
+                Chi Tiết Phòng
+              </Title>
+              <Text strong>Tên tòa nhà:</Text>
+              <Text>{` ${room.house_name}`}</Text>
+              <br />
+              <Text strong>Tên phòng:</Text>
+              <Text>{` ${room.room_id}`}</Text>
+              <br />
+              <Text strong>Loại phòng:</Text>
+              <Text>{` ${room.category}`}</Text>
+              <br />
+              <Text strong>Tài nguyên:</Text>
+              <ul>
+                {room.detail.map((item, index) => (
+                  <li key={index}>{`${item.name}: ${item.total}`}</li>
+                ))}
+              </ul>
+              <Text strong>Ghi chú:</Text>
+              <Text>{` ${room.note}`}</Text>
+            </Col>
+          </Row>
+          <Divider />
+          <Row>
+            <Col span={8} style={{ textAlign: "center" }}>
+              <Text strong>Ký tên:</Text>
+              <br />
+              <Text>{user.full_name}</Text>
+              <br />
+              <Text>(Bên cho thuê)</Text>
+            </Col>
+            <Col span={8}></Col>
+            <Col span={8} style={{ textAlign: "center" }}>
+              <Text strong>Ký tên:</Text>
+              <br />
+              <Text>{customer.full_name}</Text>
+              <br />
+              <Text>(Bên thuê phòng)</Text>
+            </Col>
+          </Row>
 
-        <Divider />
-        <Col span={24} className="receipt">
-          <Text strong>
-            Nếu bạn có phản hồi hoặc cần trợ giúp, vui lòng liên hệ tại <Link to="/feedback">đây</Link>.
-          </Text>
-          <Col className="qr-code-custom">
-            <QRCode value={qrDataFeedback} size={50} />
+          <Divider />
+          <Col span={24} className="receipt">
+            <Text strong>
+              Nếu bạn có phản hồi hoặc cần trợ giúp, vui lòng liên hệ tại <Link to="/feedback">đây</Link>.
+            </Text>
+            <Col className="qr-code-custom">
+              <QRCode value={qrDataFeedback} size={50} />
+            </Col>
           </Col>
-        </Col>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
+    </div>
   );
 };
 
